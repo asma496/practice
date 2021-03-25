7d17c72712c5a2fbe3dd206fca2cbb96a3e4d6d2
asma496 <asmaashraf496@gmail.com>
2021-03-25 11:30:17 +0500

    'practice'

---
+++ src/components/User.js
@@ -0,0 +1,42 @@
+import React, { createContext } from 'react'
+
+function User() {
+    return (
+        <div>
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+          <h1> my app</h1> 
+
+        </div>
+    )
+}
+
+export default User
